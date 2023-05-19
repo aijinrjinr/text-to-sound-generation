4f7115668d6029e462cad66d98e0071c08699c75
aijinrjinr <72067388+aijinrjinr@users.noreply.github.com>
2023-05-18 19:49:21 -0700

    Add files via upload

---
+++ Diffsound/start_yu/start.sh
@@ -0,0 +1,3 @@
+#python /apdcephfs/share_1316500/donchaoyang/code3/DiffusionFast/running_command/run_train_vgg.py
+python /apdcephfs/share_1316500/donchaoyang/code3/DiffusionFast/running_command/run_train_caps_pre_vgg.py
+#python /apdcephfs/share_1316500/donchaoyang/code3/DiffusionFast/running_command/run_train_audioset.py
